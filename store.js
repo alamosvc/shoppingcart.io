0af1968a35c4a22c6ac7b344c635b8858f98742f
alamosvc <132236798+alamosvc@users.noreply.github>
2023-05-02 11:57:43 -0300

    ya me fui

--- store.js
+++ store.js
@@ -6,30 +6,38 @@ function ready(){
     console.log(removeCartItemButtons)
     for(var i= 0; i<removeCartItemButtons.length;i++){
         var button = removeCartItemButtons[i]
-        button.addEventListener('click',function(event){
-            var buttonClicked= event.target
-            buttonClicked.parentElement.parentElement.remove()
-
-        })
+        button.addEventListener('click',removeCartItem)
+    }
+    var quantityInputs=document.getElementsByClassName('cart-item-input')
+    for(var i=0; i>quantityInputs.length;i++){       
+        input.addEventListener('change', quantityChanged)
     }
 }
+
 function removeCartItem(event){
     var buttonClicked=event.target
     buttonClicked.parentElement.parentElement.remove()
     updateCartTotal()
 }
+function quantityChanged(event){
+    var input=event.target
+    if(isNaN(input.value)|| input.value<=0){
+        input.value=1
+
+    }
+    updateCartTotal()
+}
 function updateCartTotal(){
-    var cartItemContainer=document.getElementsByClassName('cart-items')[0]
-    var cartRows= cartItemContainer.getElementsByClassName('cart-row')
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
     var total=0
     for(var i= 0; i<cartRows.length;i++){
-       var cartRow=cartRows[i]
-       var priceElement=cartRow.getElementsByClassName('cart-price')[0]
-       var quantityElement=cartRow.getElementsByClassName('cart-quantity-input')[0]
-       var price=parseFloat(priceElement.innerText.replace('$',''))
-       var quantity=quantityElement.value 
-      total=total+(price*quantity) 
-
-        })
+        var cartRow=cartRows[i]
+        var priceElement=cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement=cartRow.getElementsByClassName('cart-quantity-input')[0]
+        var price=parseFloat(priceElement.innerText.replace('$',''))
+        var quantity=quantityElement.value 
+        total=total+(price*quantity) 
+    }
         document.getElementsByClassName('cart-total-price')[0].innerText= '$' + total
 }
